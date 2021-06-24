b4416c0f0889990974945400110a8709a0b85e48
YoungHugii <79525842+Hugii-20@users.noreply.github.com>
2021-06-24 21:03:45 +0200

    Add files via upload

--- SL.bat
+++ SL.bat
@@ -1,15 +1,6 @@
-RD /S /Q %temp%
-MKDIR %temp%
-takeown /f "%temp%" /r /d y
-takeown /f "C:\Windows\Temp" /r /d y
-RD /S /Q C:\Windows\Temp
-MKDIR C:\Windows\Temp
-
-DEL /S /Q "%TMP%\*.*"
-DEL /S /Q "%TEMP%\*.*"
-DEL /S /Q "%WINDIR%\Temp\*.*"
-DEL /S /Q "%USERPROFILE%\Local Settings\Temp\*.*"
-DEL /S /Q "%LOCALAPPDATA%\Temp\*.*"
+del /f /s /q "C:\Windows\Prefetch"
+del /f /s /q "C:\Users\%username%\Appdata\Local\Temp"
+del /f /s /q "C:\Windows\Temp"
 
 PowerShell Clear-RecycleBin -Confirm:$false
 @echo off
@@ -19,8 +10,6 @@ del *.log /a /s /q /f
 @echo off
 net stop wuauserv
 net stop UsoSvc
-net stop bits
-net stop dosvc
 rd /s /q C:\Windows\SoftwareDistribution
 md C:\Windows\SoftwareDistribution
 ipconfig /flushdns
